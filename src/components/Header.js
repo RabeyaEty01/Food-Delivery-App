b7f92b451ac19d02b1833e0bb4cb950e28906c57
rabeyaety01 <etyrabeya01@gmail.com>
2022-06-06 23:21:56 +0600

    context api added and state managed by reeducer

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,8 +1,10 @@
 import { getAuth, GoogleAuthProvider, signInWithPopup } from "firebase/auth";
 import { motion } from "framer-motion";
-import React from "react";
-import { MdShoppingBasket } from "react-icons/md";
+import React, { useState } from "react";
+import { MdAdd, MdLogout, MdShoppingBasket } from "react-icons/md";
 import { Link } from "react-router-dom";
+import { actionType } from "../context/reducer";
+import { useStateValue } from "../context/StateProvider";
 import { app } from "../firebase.config";
 import profile from "../img/avatar.png";
 import Logo from "../img/logo.png";
@@ -10,10 +12,24 @@ import Logo from "../img/logo.png";
 const Header = () => {
   const firebaseAuth = getAuth(app);
   const provider = new GoogleAuthProvider();
-  console.log(process.env.REACT_APP_FIREBASE_APP_ID)
+
+  const [{ user }, dispatch] = useStateValue();
+  const [isMenu, setIsMenu] = useState(false);
+  //firebase googleLogin
   const login = async () => {
-    const response = await signInWithPopup(firebaseAuth, provider);
-    console.log(response);
+    if (!user) {
+      const {
+        user: { refreshToken, providerData },
+      } = await signInWithPopup(firebaseAuth, provider);
+      dispatch({
+        type: actionType.SET_USER,
+        user: providerData[0],
+      });
+
+      localStorage.setItem("user", JSON.stringify(providerData[0]));
+    } else {
+      setIsMenu(!isMenu);
+    }
   };
 
   return (
@@ -24,6 +40,7 @@ const Header = () => {
           <img src={Logo} alt="logo" className="w-8 object-cover" />
           <p className="text-headingColor text-xl  font-bold italic">Foodies</p>
         </Link>
+
         <div className="flex items-center gap-8">
           <ul className="flex items-center gap-8">
             <li className="text-base text-textColor hover:text-headingColor cursor-pointer duration-100 transition-all ease-in-out">
@@ -51,10 +68,29 @@ const Header = () => {
             <motion.img
               whileTap={{ scale: 0.6 }}
               onClick={login}
-              src={profile}
+              src={user ? user.photoURL : profile}
               alt="userProfile"
-              className="w-10 min-w-[40px] h-10-[40px]  drop-shadow-xl cursor-pointer"
+              className="w-10 min-w-[40px] h-10-[40px]  drop-shadow-xl cursor-pointer rounded-full"
             />
+            {isMenu && (
+              <motion.div
+                initial={{ opacity: 0, scale: 0.6 }}
+                animate={{ opacity: 1, scale: 1 }}
+                exit={{ opacity: 0, scale: 0.6 }}
+                className="w-40 bg-gray-50 shadow-xl rounded-lg flex flex-col absolute top-12 right-0"
+              >
+                {user && user.email === "etyrabeya01@gmail.com" && (
+                  <Link to={"/createItem"}>
+                    <p className="px-4 py-2 flex items-center gap-3 cursor-pointer hover:bg-slate-100 transition-all duration-100 ease-in-out text-textColor text-base">
+                      New Item <MdAdd />
+                    </p>
+                  </Link>
+                )}
+                <p className="px-4 py-2 flex items-center gap-3 cursor-pointer hover:bg-slate-100 transition-all duration-100 ease-in-out text-textColor text-base">
+                  LogOut <MdLogout />
+                </p>
+              </motion.div>
+            )}
           </div>
         </div>
       </div>
--- src/index.js
+++ src/index.js
@@ -1,13 +1,18 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import React from "react";
+import ReactDOM from "react-dom/client";
+import App from "./App";
+import { initialState } from "./context/initialState";
+import reducer from "./context/reducer";
+import { StateProvider } from "./context/StateProvider";
+import "./index.css";
+import reportWebVitals from "./reportWebVitals";
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
+const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
-    <App />
+    <StateProvider initialState={initialState} reducer={reducer}>
+      <App />
+    </StateProvider>
   </React.StrictMode>
 );
 
